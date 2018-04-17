f1d5b59380e7c8db9c8621f768ce7a47994e1bda
Ibrahim J <36282074+IntelligentBeard@users.noreply.github.com>
2018-04-17 16:43:35 -0400

    First Commit

---
+++ 05hw/alist.c
@@ -0,0 +1,17 @@
+// Copyright 2018 Ibrahim
+
+#include <stdlib.h>
+#include <assert.h>
+#include "alist.h"
+
+//
+alist_t alalloc(void) {
+    alist_t *list = calloc(1, sizeof(alist_t));
+    assert(list != NULL);
+    list->arr = calloc(DEFAULT_LEN, 0);
+    list->len = DEFAULT_LEN;
+    list->size = 0;
+    return list;
+}
+
+
