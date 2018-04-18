b14634de87d5a2dd6e886d0181afcdef914e0c1d
Ibrahim J <36282074+IntelligentBeard@users.noreply.github.com>
2018-04-18 15:54:09 -0400

    added the push, pop, find and map methods. Not sure if they totaly work though

--- 05hw/alist.c
+++ 05hw/alist.c
@@ -5,12 +5,48 @@
 #include "alist.h"
 
 alist_t* alalloc(void) {
-    alist_t *list = calloc(1, sizeof(alist_t));
+    alist_t *list = malloc(sizeof *list);
     assert(list != NULL);
-    list->arr = calloc(DEFAULT_LEN, 0);
-    list->len = DEFAULT_LEN;
     list->size = 0;
+    list->len = DEFAULT_LEN;
+    list->arr = calloc(DEFAULT_LEN, sizeof(void *));
+    assert(list->arr != NULL);
     return list;
 }
 
+void push(alist_t* lst, void *item) {
+    // Hidden Code
+    // Hidden Code
+    // Hidden Code
+    // Hidden Code
+    // Hidden Code
+    // Hidden Code
+}
 
+void* pop(alist_t *lst){
+    // Hidden Code
+    // Hidden Code
+    // Hidden Code
+    // Hidden Code
+    // Hidden Code
+}
+
+void* find(alist_t *lst, bool (*sel)(void*)){
+   // Hidden Code
+   // Hidden Code
+   // Hidden Code
+   // Hidden Code
+   // Hidden Code
+   // Hidden Code
+   // Hidden Code
+   // Hidden Code
+   // Hidden Code
+}
+void map(alist_t *lst, void* (*func)(void*)){
+    // Hidden Code
+    // Hidden Code
+    // Hidden Code
+    // Hidden Code
+    // Hidden Code
+    // Hidden Code
+}
